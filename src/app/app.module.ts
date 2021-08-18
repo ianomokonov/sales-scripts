56e5b92b9425cf545c30c4a92f20c01a2683df45
volik25 <i.a.volik@gmail.com>
2021-08-18 20:32:18 +0300

    task: VolikIA/Sale-4 Добавлена библиотека PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { PanelModule } from 'primeng/panel';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, PanelModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
