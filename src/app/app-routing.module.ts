7c8861e888803ce0e154ac3a903e46821a71cf14
volik25 <i.a.volik@gmail.com>
2021-08-20 21:40:50 +0300

    task: VolikIA/Sale-8 Шаблон форм входа и регистрации

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './_components/sign-in/sign-in.component';
+import { SignUpComponent } from './_components/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
     path: 'panel',
     loadChildren: () =>
-      import('./dialog-panel/dialog-panel.module').then((m) => m.DialogPanelModule),
+      import('./_modules/dialog-panel/dialog-panel.module').then((m) => m.DialogPanelModule),
   },
   {
     path: 'profile',
     loadChildren: () =>
-      import('./personal-cabinet/personal-cabinet.module').then((m) => m.PersonalCabinetModule),
+      import('./_modules/personal-cabinet/personal-cabinet.module').then(
+        (m) => m.PersonalCabinetModule,
+      ),
+  },
+  {
+    path: 'sign-in',
+    component: SignInComponent,
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'sign-in',
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { PanelModule } from 'primeng/panel';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './_components/sign-in/sign-in.component';
+import { SignUpComponent } from './_components/sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, PanelModule, ButtonModule],
+  declarations: [AppComponent, SignInComponent, SignUpComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    InputTextModule,
+    FormsModule,
+    CardModule,
+    ButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
