9fb0809b19c03a65701a521a70fbe7d716b559c3
Ivan Nomokonov <nomokonov.vana@gmail.com>
2021-08-28 00:43:34 +0300

    SALE-19: форматирование

--- src/app/modules/personal-cabinet/personal-cabinet-routing.module.ts
+++ src/app/modules/personal-cabinet/personal-cabinet-routing.module.ts
@@ -10,9 +10,9 @@ const routes: Routes = [
     children: [
       {
         path: 'script/:id',
-        component: SaleScriptComponent
-      }
-    ]
+        component: SaleScriptComponent,
+      },
+    ],
   },
 ];
 
--- src/app/modules/personal-cabinet/sale-script/sale-script.component.ts
+++ src/app/modules/personal-cabinet/sale-script/sale-script.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sale-script',
   templateUrl: './sale-script.component.html',
-  styleUrls: ['./sale-script.component.less']
+  styleUrls: ['./sale-script.component.less'],
 })
-export class SaleScriptComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class SaleScriptComponent {}
