8f9ff4c8d39d4681d9446de10e1b623e5acd4d42
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2021-08-30 23:02:48 +0300

    access

--- back/index.php
+++ back/index.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: https://stand1.progoff.ru/api");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization");
 
